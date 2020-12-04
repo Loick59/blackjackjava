d0f248e35efea149c03c28fe344f3cd9039a3511
Loïck FREMY <loick.fremy@P210.sn>
2020-12-04 11:48:22 +0100

    j++;cartes

--- blackjack/Deck.java
+++ blackjack/Deck.java
@@ -1,5 +1,25 @@
 package blackjack;
 
 public class Deck {
+	Card[] content = new Card[52];
 
+	public Deck() {
+		int j = 0;
+		for (int i = 1; i < 14; i++) {
+			for (int a = 0; a < 4; a++) {
+				this.content[j] = new Card(a, i);
+				j++;
+			}
+			
+		}
+	}
+
+	@Override
+	public String toString() {
+		String result = " ";
+		for (int b = 0; b < 52; b++) {
+			result = result + (content[b] + ", ");
+		}
+		return result;
+	}
 }
--- blackjack/card.java
+++ blackjack/card.java
@@ -3,30 +3,30 @@ package blackjack;
 public class Card {
 	public char color;
 	public int value;
-	public Card(int c,int v) {
+
+	public Card(int c, int v) {
 		this.value = v;
 		switch (c) {
-		case 0 :
+		case 0:
 			this.color = 'H';
 			break;
-		case 1 :
+		case 1:
 			this.color = 'D';
 			break;
-		case 2 :
+		case 2:
 			this.color = 'S';
 			break;
-		case 3 :
+		case 3:
 			this.color = 'C';
 			break;
-		
-		} 
+
+		}
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public String toString() {
-		return "" + this.color +this.value;
+		return "" + this.color + this.value;
 	}
-	
-	
+
 }
