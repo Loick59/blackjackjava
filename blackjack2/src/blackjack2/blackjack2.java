1743733d68c62eb7be35e263894aea714317f6de
Loick59 <71765674+Loick59@users.noreply.github.com>
2020-12-08 23:43:46 +0100

    mardi soir

---
+++ blackjack2/src/blackjack2/blackjack2.java
@@ -0,0 +1,10 @@
+package blackjack2;
+
+public class blackjack2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Deck d = new Deck();
+		d.shuffle();
+		System.out.println(d);
+	}
+}
